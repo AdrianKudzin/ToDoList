241fd177f34f359460c81fe415194b0170239563
Adrian Kudzin <adrian.kudzin@tlen.pl>
2023-09-06 16:29:29 +0200

    remove tasks

--- js/todolist.js
+++ js/todolist.js
@@ -10,26 +10,39 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li class="section__listItem${task.done ? " section__listItem--done" : ""}">
-                    ${task.content}
-                </li>
-            `;
-        };
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li class="section__listItem${task.done ? " section__listItem--done" : ""}">
+                    ${task.content} <button class="section__button--done js-remove">usuń</button>
+                </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
     const onFormSubmit = (event) => {
