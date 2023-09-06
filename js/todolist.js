43f5c4c20e8cbcd4da53f9a7a481ed03d076d979
Adrian Kudzin <adrian.kudzin@tlen.pl>
2023-09-06 16:07:46 +0200

    adding new tasks

--- js/todolist.js
+++ js/todolist.js
@@ -13,9 +13,9 @@
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
-                <li class="section__listItem">
+                <li class="section__listItem${task.done ? " section__listItem--done" : ""}">
                     ${task.content}
                 </li>
             `;
@@ -23,8 +23,33 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
